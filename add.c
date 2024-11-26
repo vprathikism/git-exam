cad9750c0b9858948047b4b619a84e16b808ec9f
vprathik <vemsaiprathikreddy@gmail.com>
2024-11-26 23:17:46 +0530

    Loops

--- add.c
+++ add.c
@@ -3,12 +3,19 @@ int main()
 {
  int a, b;
  int sum;
+ char confirm = 'n';
+ do
+ {
  printf("Enter first integer: ");
  scanf("%d", &a);
  printf("Enter second integer: ");
  scanf("%d", &b);
  sum = a + b;
  printf("The sum of %d and %d is %d\n", a, b, sum);
+ printf("Do you have more sums for me [y/N]? ");
+ scanf(" %c", &confirm);
+ }
+ while(confirm == 'y' || confirm == 'Y');
  return 0;
 }
 
