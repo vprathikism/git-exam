bd2f473b45258ade942754ba43dc9579bdba89f5
vprathik <vemsaiprathikreddy@gmail.com>
2024-11-26 23:14:19 +0530

    implemented

--- add.c
+++ add.c
@@ -1,14 +1,25 @@
 #include<stdio.h>
+int add(int i, int j)
+{
+ return (i + j);
+}
 int main()
 {
  int a, b;
  int sum;
+ char confirm = 'n';
+ do
+ {
  printf("Enter first integer: ");
  scanf("%d", &a);
  printf("Enter second integer: ");
  scanf("%d", &b);
- sum = a + b;
+ sum = add(a, b);
  printf("The sum of %d and %d is %d\n", a, b, sum);
+ printf("Do you have more sums for me [y/N]? ");
+ scanf(" %c", &confirm);
+ }
+ while(confirm == 'y' || confirm == 'Y');
  return 0;
 }
 
